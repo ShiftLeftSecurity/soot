973b622fde9a03e0e2065f7cec191b195383f6eb
Eric Bodden <bodden@ec-spride.de>
2007-09-12 18:09:07 +0000

    reverting to revision 3026; previous changes were unfortunately incorrect

--- tmopt-mustalias/abc/trunk/src/abc/tm/weaving/weaver/tmanalysis/mustalias/ShadowSideEffectsAnalysis.java
+++ tmopt-mustalias/abc/trunk/src/abc/tm/weaving/weaver/tmanalysis/mustalias/ShadowSideEffectsAnalysis.java
@@ -29,17 +29,14 @@ import soot.Scene;
 import soot.SootMethod;
 import abc.tm.weaving.aspectinfo.TraceMatch;
 import abc.tm.weaving.matching.SMNode;
-import abc.tm.weaving.matching.State;
-import abc.tm.weaving.weaver.tmanalysis.query.PathInfoFinder;
 import abc.tm.weaving.weaver.tmanalysis.query.ShadowGroup;
 import abc.tm.weaving.weaver.tmanalysis.query.ShadowGroupRegistry;
 import abc.tm.weaving.weaver.tmanalysis.query.SymbolShadowWithPTS;
-import abc.tm.weaving.weaver.tmanalysis.query.PathInfoFinder.PathInfo;
-import abc.tm.weaving.weaver.tmanalysis.query.PathInfoFinder.StatePredicate;
 
 
 /**
- * Shadow side-effects analyis.
+ * IntraproceduralAnalysis: This analysis propagates tracematch
+ * automaton states through the method.
  *
  * @author Eric Bodden
  */
@@ -48,7 +45,7 @@ public class ShadowSideEffectsAnalysis  {
 	/**
 	 * TODO RENAME AND COMMENT
 	 */
-	public boolean allShadowsWithOverLappingBindingInSameMethod(String tmVar, Local toBind, SootMethod container, TraceMatch tm, final SMNode from) {
+	public boolean allShadowsWithOverLappingBindingInSameMethod(String tmVar, Local toBind, SootMethod container, TraceMatch tm, SMNode from) {
 	    if(!ShadowGroupRegistry.v().hasShadowGroupInfo()) {
 	        return false;
 	    }
@@ -69,48 +66,21 @@ public class ShadowSideEffectsAnalysis  {
 		//exclude all shadows from the given container  and artificial shadows
 		for (Iterator<SymbolShadowWithPTS> shadowIter = overlaps.iterator(); shadowIter.hasNext();) {
 		    SymbolShadowWithPTS shadow = (SymbolShadowWithPTS) shadowIter.next();
-		    
-		    /* FIXME We have a problem here:
-		     * Including "shadow.getContainer().equals(container) ||" is actually necessary for precision
-		     * but in general it's unsound. This is a case where things go wrong:
-		     * - one method with create(c,i)
-		     * - one method with update(c) -> current method
-		     * - one method with next(i)
-		     *
-		     * Problem: The update(c) shadow generates a binding (c=c) on the second-last state.
-		     * The variable i is here unbound. The artificial final node would normally propagate
-		     * this disjunct to the final state using the next(i) shadow. However, since we remove
-		     * update(c) from "overlaps", only the create and next shadow remain, not satisfying
-		     * the path info for the second-last state (because the "update" is missing). However,
-		     * if we generally include all shadows from the current method, this is too coarse grain.
-		     * 
-		     * I think we need a somewhat other abstraction.
-		     * 
-		     * (Eric 17/08/07)
-		     */
-            if(/*shadow.getContainer().equals(container) ||*/ shadow.isArtificial()) {
+            if(shadow.getContainer().equals(container) || shadow.isArtificial()) {
                 shadowIter.remove();
             }
         }
 		
-		if(overlaps.isEmpty()) {
-			return true;
-		}
+		Set<String> variablesBoundByShadows = new HashSet<String>();
+		for (SymbolShadowWithPTS shadow : overlaps) {
+            variablesBoundByShadows.addAll(shadow.getBoundTmFormals());
+        }
 		
-	    Set<PathInfo> pathInfos = new PathInfoFinder(tm, new StatePredicate() {
-
-			public boolean match(State s) {
-				return s==from;
-			}
-	    	
-	    }).getPathInfos();
-	    
-	    for (PathInfo pathInfo : pathInfos) {
-			if(pathInfo.isSatisfiedByShadowSet(overlaps)) {
-				return false;
-			}
-		}
-	    return true;
+		if(!variablesBoundByShadows.containsAll(from.boundVars)) {
+		    return true;
+		} else {
+		    return false;
+		}		
 	}
 
 	protected PointsToSet getPointsToSetOf(Local toBind) {
