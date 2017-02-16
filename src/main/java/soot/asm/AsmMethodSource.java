0844b080fa021cee5e9374d0682e11197b87666e
Markus Lottmann <markus@shiftleft.io>
2017-02-16 15:14:41 +0100

    Fixed parameter type ordering of dynamic invoke instructions.
    The parameter types were in reverse order which let SOOT to create
    invalid cast expressions and sometimes to emitting exceptions.

--- src/main/java/soot/asm/AsmMethodSource.java
+++ src/main/java/soot/asm/AsmMethodSource.java
@@ -1280,7 +1280,7 @@ final class AsmMethodSource implements MethodSource {
 			DynamicInvokeExpr indy = Jimple.v().newDynamicInvokeExpr(bsmMethodRef,
 					bsmMethodArgs, methodRef, insn.bsm.getTag(), methodArgs);
 			
-			for (int i = 0; i < args.length - 1; i++) {
+			for (int i = 0; i < args.length; i++) {
 				boxes[i] = indy.getArgBox(i);
 				args[i].addBox(boxes[i]);
 			}
