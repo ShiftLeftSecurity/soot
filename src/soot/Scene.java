156ff2baf95b6c83a3431363dfc16c5a14a43fdc
Markus Lottmann <markus@shiftleft.io>
2017-04-20 11:15:38 +0200

    New method references are now always resolved.
    Reason:
    Methods not found in one of the class files loaded in the SOOT classpath
    are only returned by SootClass::getMethods() if they were resolved in
    advance.

--- src/soot/Scene.java
+++ src/soot/Scene.java
@@ -1696,8 +1696,10 @@ public class Scene  //extends AbstractHost
             List<Type> parameterTypes,
             Type returnType,
             boolean isStatic ) {
-        return new SootMethodRefImpl(declaringClass, name, parameterTypes,
-                returnType, isStatic);
+        SootMethodRef sootMethodRef =
+				new SootMethodRefImpl(declaringClass, name, parameterTypes, returnType, isStatic);
+        sootMethodRef.resolve();
+        return sootMethodRef;
     }
 
     /** Create an unresolved reference to a constructor. */
