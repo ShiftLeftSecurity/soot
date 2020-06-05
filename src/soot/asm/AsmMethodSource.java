d936a9b532c23d7d57747160dd3d324d268a25bc
Markus Lottmann <markus@shiftleft.io>
2020-06-05 14:37:56 +0200

    Use worklist instead of recursive decent in emitUnits.
    This is to avoid call stack limit exceptions.
    
    Also add loop detection to make sure we do not get stuck due to
    loops in the units referencing each other.

--- src/soot/asm/AsmMethodSource.java
+++ src/soot/asm/AsmMethodSource.java
@@ -1777,8 +1777,30 @@ final class AsmMethodSource implements MethodSource {
 	
 	private void emitUnits(Unit u) {
 		if (u instanceof UnitContainer) {
-			for (Unit uu : ((UnitContainer) u).units)
-				emitUnits(uu);
+			ArrayDeque<Unit> worklist = new ArrayDeque<>();
+			// The set is here only for the case there are loops in the unit container which
+			// would be a bug. But with SOOT you never know.
+			HashSet<Unit> alreadyProcessedUnits = new HashSet<>();
+			worklist.addFirst(u);
+
+			while (!worklist.isEmpty()) {
+				Unit currentUnit = worklist.removeFirst();
+				if (alreadyProcessedUnits.contains(currentUnit)) {
+					continue;
+				}
+
+				alreadyProcessedUnits.add(currentUnit);
+
+				if (currentUnit instanceof UnitContainer) {
+					Unit units[] = ((UnitContainer) currentUnit).units;
+					// To maintain ordering we need to add in reverse.
+					for (int i = units.length - 1; i >=0; i--) {
+						worklist.addFirst(units[i]);
+					}
+				} else {
+					body.getUnits().add(currentUnit);
+				}
+			}
 		} else {
 			body.getUnits().add(u);
 		}
